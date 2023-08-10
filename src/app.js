e799a271e3b7e2ea92184fc0a53140218a275539
Genesis Longart <102156923+GenyLongart@users.noreply.github.com>
2023-08-10 16:53:38 +0000

    Agregando validación

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+const alertMessage = document.querySelector(".alert");
+alertMessage.style.display = "none";
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const form = document.getElementById("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // arr para guardar campos con errores
+    const emptyInputs = [];
+
+    const inp = form.querySelectorAll("input, select");
+    inp.forEach(field => {
+      if (field.value === "") {
+        emptyInputs.push(field);
+        field.classList.add("alert", "alert-danger");
+      } else {
+        field.classList.remove("alert", "alert-danger");
+      }
+    });
+
+    // alerta
+    if (emptyInputs.length > 0) {
+      alertMessage.style.display = "block";
+      alertMessage.textContent =
+        "Por favor asegúrese de rellenar o corregir los siguientes campos resaltados ";
+    } else {
+      form.submit();
+    }
+  });
 };
